b50d4c841749b13a5df9f9bb4cd94db5b77e50da
kevin <mattisonii@yahoo.com>
2016-03-18 10:55:55 -0700

    Start js and jqeury content.

---
+++ js/browser-interface.js
@@ -0,0 +1,9 @@
+var apiKey = require('./../inv').apiKey;
+
+$(document).ready(function() {
+  $('#searchGit').click(function() {
+    var gitName = $("#searchGitName").val();
+    $('#searchGitName').val("");
+    $('')
+  })
+})
