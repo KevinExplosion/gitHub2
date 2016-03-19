47e614bfcff6e27ff1b5615b4750e6963501d92a
kevin <mattisonii@yahoo.com>
2016-03-18 17:34:52 -0700

    Make JS changes. Still not working

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,13 +1,16 @@
 var apiKey = require('./../.env').apiKey;
 var gitName;
+var login;
 
 $(document).ready(function() {
   $('#searchButton').click(function() {
+    event.preventDefault();
+    
     var gitName = $("#searchGit").val();
     $('#searchGit').val("");
     $.get('https://api.github.com/users/'+ gitName +'?access_token=' + apiKey).then(function(response){
       // console.log(response);
-      $('.showGitName').text("Repositories for");
+      $('.showGitName').text("Repositories for" +response.owner.login);
       console.log(response);
     }).fail(function(error){
       console.log(error.responseJSON.message);
