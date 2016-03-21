08a21197fbfd6fe7f1519915d86fd0ee477bef3f
Kevin Mattison <mattisonii@yahoo.com>
2016-03-20 18:33:36 -0700

    Make changes to gitHub.js

--- js/gitHub.js
+++ js/gitHub.js
@@ -1,9 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    for (i=0, i<response.length, i++) {
+      $('#showGitName').html("<li>Repository: "+response[i].name+ "</li>");
+      $('#showGitName').html("<li>Description: "+response[i].description+ "</li>");
+    }
   }).fail(function(error){
+
     console.log(error.responseJSON.message);
   });
 };
